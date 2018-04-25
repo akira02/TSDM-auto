e9028eb3872f1f242f2fe955930a58b3f242005e
八雲 千秋 <akiakira02@gmail.com>
2018-04-25 16:37:05 +0800

    Make myID local var

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 setInterval(open, 21600000+180000);
-var myID;
 
 function open(){
+    var myID;
     chrome.tabs.create({"url": "http://www.tsdm.me/plugin.php?id=np_cliworkdz:work","active":false},function(tab){
         myID = tab.id
     })
     setTimeout(close,600000)
+    function close(){
+        chrome.tabs.remove(myID);
+    }
 }
 
-function close(){
-        chrome.tabs.remove(myID);
-}
